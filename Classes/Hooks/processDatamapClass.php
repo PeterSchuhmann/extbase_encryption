c354182484ae4e67a0db2e912458a7e0328a8e9e
Peter Schuhmann <mail@peterschuhmann.de>
2020-03-27 14:34:43 +0100

    removed old code

--- Classes/Hooks/processDatamapClass.php
+++ Classes/Hooks/processDatamapClass.php
@@ -8,27 +8,7 @@ class processDatamapClass {
 
     public function processDatamap_afterDatabaseOperations($status, $table, $id, array $fieldArray, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj)
     {
-        if (
-            is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase_encryption']['classes']) &&
-            count($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase_encryption']['classes']) > 0 &&
-            isSet($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase_encryption']['classes'][$table])
-        ) {
-            $encryptor = Encryptor::init();
-
-            $row = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($table, intval($id));
-
-            $row = $encryptor->encryptRow($row, $table);
-
-            $oldVersion = (VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 8007000);
-
-            if ($oldVersion) {
-                $GLOBALS['TYPO3_DB']->exec_UPDATEquery($table, 'uid = ' . $id, $row);
-            }
-            else {
-                // needs implementation
-            }
-
-        }
+        // deprecated
 
     }
 
